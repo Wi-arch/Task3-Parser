b710abfa7f10bed6718ef952047c6713b3ade8a3
Alexandr Borovets <borovets9494@mail.ru>
2020-02-24 10:03:29 +0300

    Add validation in parseString method in SymbolParser

--- src/main/java/by/training/parser/service/parser/SymbolParser.java
+++ src/main/java/by/training/parser/service/parser/SymbolParser.java
@@ -7,11 +7,12 @@ import by.training.parser.exception.ServiceException;
 public class SymbolParser implements Parser {
 
 	private static final int INDEX_OF_FIRST_SYMBOL = 0;
+	private static final int VALID_STRING_LENGTH = 1;
 
 	@Override
 	public Component parseString(String string) throws ServiceException {
-		if (string == null) {
-			throw new ServiceException("Cannot add null symbol");
+		if (string == null || string.length() != VALID_STRING_LENGTH) {
+			throw new ServiceException("Cannot add symbol");
 		}
 		return new Leaf(string.charAt(INDEX_OF_FIRST_SYMBOL));
 	}
